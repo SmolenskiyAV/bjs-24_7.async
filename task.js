0e5c63ac439ddf669af323c7a7633877afb9a0b0
SmolenskiyAV <JoJo_666@list.ru>
2021-10-22 01:17:09 +0700

    Версия 0.3

--- task.js
+++ task.js
@@ -3,7 +3,7 @@ class AlarmClock {
     constructor() {
         
         this.alarmCollection = []; // коллекция звонков
-        this.timerId = NaN; // свойство для хранения id без начального состояния
+        this.timerId = null; // свойство для хранения id без начального состояния
 
     }
 
@@ -17,59 +17,72 @@ class AlarmClock {
         }
 
         return this.alarmCollection.push(
-            {id: id, time: time, callback: callback} // добавление в коллецию звонков объекта со свойствами
+            {id, time, callback} // добавление в коллецию звонков объекта со свойствами
         )
     }
 
     removeClock(id) {
 
-        let preLength = this.alarmCollection; // длина массива до удаления
+        let preLength = this.alarmCollection.length; // длина массива до удаления
 
         const indexResult = this.alarmCollection.findIndex((object) => object.id === id); //находим индекс элемента со свойством, равным id
         this.alarmCollection.splice(indexResult, 1); // удаление найденного элемента
 
-        return  (preLength - this.alarmCollection !==0); // если удаление успешно, возвращаем true
+        return  (preLength - this.alarmCollection.length !==0); // если удаление успешно, возвращаем true
 
     }
 
     getCurrentFormattedTime() {
         
         let nowDate = new Date(); // получение текущего времени в стандартном формате
-        return (nowDate.getHours() + ':' + nowDate.getMinutes()); // вывод строкового значения текущего времени в формате НН:ММ
-      
+        return (
+            nowDate.toLocaleTimeString("ru-Ru", {// вывод строкового значения текущего времени в формате НН:ММ
+                hour: "2-digit",
+                minute: "2-digit",
+                })
+        )
     }
 
     start() {
         
         function checkClock(time, callback) { //получение звонка функцией в качестве двух параметров
             
-            if (time === getCurrentFormattedTime()) callback; //если time совпадает с текущим временем, вызываем callback
-        }
-
-            if (this.timerId === NaN) {
-                setInterval(this.alarmCollection.forEach(checkClock)) // перебор коллекции звонков и вызов checkClock. 
-                this.timerId = setInterval(this.alarmCollection.forEach(checkClock)) // Сохранение результата setInterval в свойство идентифиатора текущего таймера
+            if (time === this.getCurrentFormattedTime()) callback(); //если time совпадает с текущим временем, вызываем callback
+        
+            if (this.timerId === null) {
+                
+                this.timerId = setInterval( // Сохранение результата setInterval в свойство идентифиатора текущего таймера
+                
+                    function (){
+                        let checkClocK = checkClock.bind(start); //пытаемся привязать функцию checkClock к контексту метода .start() текущего объекта(принадлежащего классу AlarmClock) 
+                        for (const item of this.alarmCollection) {
+                        checkClocK(); // вызываем checkClock в контексте текущего объекта
+                        }
+                    }
+                )
             }
+         }  
+                
     }
 
     stop() {
 
-        if (this.timerId !== NaN) {
+        if (this.timerId !== null) {
             clearInterval(this.timerId); // удаление интервала
-            this.timerId = NaN; //удаление значения из идентификатора текущего таймера
+            this.timerId = null; //удаление значения из идентификатора текущего таймера
         }
 
     }
 
     printAlarms() {
 
-        this.alarmCollection.forEach(console.log(this.alarmCollection.id, this.alarmCollection.time)); //вывод информации о каждом звонке 'id' и 'time'
+        this.alarmCollection.forEach((item, idx) => console.log('Будильник №' + this.alarmCollection[idx].id + ' заведён на' + this.alarmCollection[idx].time)); //вывод информации о каждом звонке
 
     }
 
     clearAlarms() {
         this.stop();
-        this.alarmCollection.splice(0, end); // очистка коллекции звонков 
+        this.alarmCollection.splice(0, (this.alarmCollection.length - 1)); // очистка коллекции звонков от 0-го до последнего элемента
     }
 
 }
